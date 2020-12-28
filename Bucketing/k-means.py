2a27a766a6e520c6ab94972875ee7a3fe16453c0
anaidde <dadam631@gmail.com>
2020-12-28 20:56:24 +0200

    done a lot
    trained all data
    almost finished json file with bucked data
    date bucket still in progress

--- Bucketing/k-means.py
+++ Bucketing/k-means.py
@@ -1,6 +1,9 @@
 import json
 import numpy as np
 import kmeans1d
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+from datetime import datetime
 
 
 def create_lists():
@@ -13,6 +16,9 @@ def create_lists():
     lc_l = []
     fac_l = []
     sc_l = []
+    rt_l = []
+    text_l = []
+    time_l = []
 
     for i in json_content["tweets"]:
         fc_l.append(i["u"]["fc"])
@@ -20,30 +26,150 @@ def create_lists():
         lc_l.append(i["u"]["lc"])
         fac_l.append(i["u"]["fac"])
         sc_l.append(i["u"]["sc"])
+        rt_l.append((i["rt"]))
+        text_l.append(i["t"])
+        time_l.append(i["ca"])
 
-    return fc_l, frc_l, lc_l, fac_l, sc_l
+    return fc_l, frc_l, lc_l, fac_l, sc_l, rt_l, text_l, time_l
+
+
+def make_array_of_int(arr):
+    int_arr = []
+    for element in arr:
+        int_arr.append(int(element))
+
+    return int_arr
+
+
+def make_json_data_file(fc, frc, lc, fac, sc, rt, txt): # ca argument, se vor da clusterele
+    json_f = open("fakeResultTweets.json", "w")
+    file_data = {"features": []}
+
+    for index in range(0, len(fc)):
+        sentiment = -1
+
+        analysis = TextBlob(txt[index]).sentiment
+        blob_obj = TextBlob(txt[index], analyzer=NaiveBayesAnalyzer())
+
+        sentiment_analysis = blob_obj.sentiment
+        print(sentiment_analysis)
+
+        print("sentiment analisys: ", sentiment_analysis[0])
+
+        objectivity = 1-analysis[1]
+
+        if sentiment_analysis[0] == 'pos':
+            sentiment = 1
+        elif sentiment_analysis[0] == 'neg':
+            sentiment = 0
+
+        file_data["features"].append({
+            "index": index,
+            "retweets": rt[index],
+            "follower_bucket": fc[index],
+            "friends_bucket": frc[index],
+            "listed_bucket": lc[index],
+            "favourites_bucket": fac[index],
+            "statuses_bucket": sc[index],
+            "sentiment": sentiment,
+            "objectivity": objectivity
+        })
+
+
+def create_time_bucket(date_list):
+    formated_date_list = []
+
+    for date in date_list:
+        date_time_obj = datetime.strptime(date, '%d/%m/%y %H:%M:%S')
+        formated_date_list.append(date_time_obj)
+
+    for d in formated_date_list:
+        print(d.hour)
 
 
 if __name__ == "__main__":
 
-    fc_list, frc_list, lc_list, fac_list, sc_list = create_lists()
+    fc_list, frc_list, lc_list, fac_list, sc_list, rt_list, text_list, time_list = create_lists()
 
     k = 11
 
     fc_array = np.array(fc_list)
     fc_clusters, fc_centroids = kmeans1d.cluster(fc_array, k)
 
+    # print("fc_plot: ", fc_array)
+    # print("fc_cluster: ", fc_clusters)
+    # print("fc_centroids: ", make_array_of_int(fc_centroids))
+
+    k = 16
+
     frc_array = np.array(frc_list)
     frc_clusters, frc_centroids = kmeans1d.cluster(frc_array, k)
+
+    # print("frc_plot: ", frc_array)
+    # print("frc_cluster: ", frc_clusters)
+    # print("frc_centroids: ", make_array_of_int(frc_centroids))
+
+    k = 7
 
     lc_array = np.array(lc_list)
     lc_clusters, lc_centroids = kmeans1d.cluster(lc_array, k)
 
+    # print("lc_plot: ", lc_array)
+    # print("lc_cluster: ", lc_clusters)
+    # print("lc_centroids: ", make_array_of_int(lc_centroids))
+
+    k = 16
+
     fac_array = np.array(fac_list)
     fac_clusters, fac_centroids = kmeans1d.cluster(fac_array, k)
 
+    # print("fac_plot: ", fac_array)
+    # print("fac_cluster: ", fac_clusters)
+    # print("fac_centroids: ", make_array_of_int(fac_centroids))
+
+    k = 11
+
     sc_array = np.array(sc_list)
     sc_clusters, sc_centroids = kmeans1d.cluster(sc_array, k)
-   
+
+    # print("sc_plot: ", sc_array)
+    # print("sc_cluster: ", sc_clusters)
+    # print("sc_centroids: ", make_array_of_int(sc_centroids))
+
+    # print(len(sc_clusters), len(fac_clusters), len(fc_clusters), len(lc_clusters), len(frc_clusters))
+
+   #  make_json_data_file(fc_list, frc_list, lc_list, fac_list, sc_list, rt_list, text_list)
+
+    create_time_bucket(time_list)
+
+#
+# {
+#     "features": [
+#         {
+#             "index": 1,
+#             "retweets": 20,
+#             "follower_bucket": 1,
+#             "friends_bucket": 1,
+#             "listed_bucket": 1,
+#             "favourites_bucket": 1,
+#             "statuses_bucket": 1,
+#             "sentiment": 0,
+#             "objectivity": 0,
+#             "time_bucket": 11,
+#         },
+#         {
+#             "index": 2,
+#             "retweets": 30,
+#             "follower_bucket": 1,
+#             "friends_bucket": 1,
+#             "listed_bucket": 1,
+#             "favourites_bucket": 1,
+#             "statuses_bucket": 1,
+#             "sentiment": 0,
+#             "objectivity": 0,
+#             "time_bucket": 12,
+#         },
+#     ]
+# }
 
 
